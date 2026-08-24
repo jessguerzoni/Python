e12fcbd138db00950f379a40e0e05346f2673e59
Jéssica Guerzoni <82246276+jessguerzoni@users.noreply.github.com>
2026-08-24 20:07:54 -0300

    Update Extra_Countries.py

--- Extra_Countries.py
+++ Extra_Countries.py
@@ -9,7 +9,7 @@ print(dataset[0:,0:4])
 
 #02 - contar e Mostrar as diferentes regioes
 
-reg_unica=(np.unique(dataset[:,1]))
+reg_unica=(np.unique(dataset[1:,1]))
 
 print("Total de regiões diferentes: {}".format(len(reg_unica)))
 
@@ -31,13 +31,15 @@ print(f"Países da América do Norte: {len(paises_north_america)}")
 
 #05
 
-america_sul_caribe = dataset[dataset[:, 1] == 'LATIN AMER. & CARIB']
+import numpy as np
 
-posicao = np.argmax(america_sul_caribe[:, 2].astype(float))
+dataset=np.loadtxt('paises.csv',delimiter=';',dtype=str,encoding='utf-8')
 
+mask = np.char.strip(dataset[:, 1]) == 'LATIN AMER. & CARIB'
+america_sul_caribe = dataset[mask]
+posicao = np.argmax(america_sul_caribe[:, 8].astype(float))
 pais = america_sul_caribe[posicao, 0]
-
-gdp = america_sul_caribe[posicao, 2]
+gdp = america_sul_caribe[posicao, 8]
 
 print(f"País com maior GDP per capita: {pais}")
 print(f"GDP per capita: {gdp}")
